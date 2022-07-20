c4f87f1c4847cbbfe27824472e9c7bdd5d8fb4e5
Konstantinos <80920175+shad0wlan@users.noreply.github.com>
2022-07-20 14:53:05 +0300

    change main bacteria.py

--- Glowing bacteria/bacteria.py
+++ Glowing bacteria/bacteria.py
@@ -34,28 +34,16 @@ def opposite_strand(strand):
     return translated_strand
 
 
-complementary_plasmid = opposite_strand(original_plasmid_strand[0])
-complementary_gfp = opposite_strand(gfp_original_strand[0])
-
-
 def gfp_cut(strand, restriction_sites):
-    complementary_strand = opposite_strand(strand[0])
-    complementary_site_one = opposite_strand(restriction_sites[0])
-    complementary_site_two = opposite_strand(restriction_sites[1])
     first_cut_index = strand[0].index(restriction_sites[0])
     second_cut_index = strand[0].index(restriction_sites[1])
     cutted_original = strand[0][first_cut_index + 1:second_cut_index + 1]
-    first_cut_comp_index = complementary_strand.index(complementary_site_one)
-    second_cut_comp_index = complementary_strand.index(complementary_site_two)
-    cutted_complementary = complementary_strand[first_cut_comp_index + len(complementary_site_one) - 1:second_cut_comp_index + len(complementary_site_two) - 1]
+
     return cutted_original
 
 
-def plasmid_cut(original, complementary, restriction_site):
-    restriction_complement = opposite_strand(restriction_site)
+def plasmid_cut(original, restriction_site):
     original_cut = original.replace(restriction_site, f'{restriction_site[0]} {restriction_site[1:]}')
-    complementary_cut = complementary.replace(restriction_complement, f'{restriction_complement[:-1]} '
-                                                                f'{restriction_complement[-1]}')
     final_original_cut = original_cut.split()
     begin = final_original_cut[0]
     end = final_original_cut[1]
@@ -63,9 +51,11 @@ def plasmid_cut(original, complementary, restriction_site):
 
 
 def main():
-    original_plasmid_cut_begin, original_plasmid_cut_end = plasmid_cut(original_plasmid_strand[0], complementary_plasmid, restriction_site_plasmid_strand[0])
+    original_plasmid_cut_begin, original_plasmid_cut_end = plasmid_cut(original_plasmid_strand[0],
+                                                                       restriction_site_plasmid_strand[0])
     gfp_original_cut = gfp_cut(gfp_original_strand, gfp_restriction_sites)
-    original_glowing, complementary_glowing = ligation(original_plasmid_cut_begin, gfp_original_cut, original_plasmid_cut_end)
+    original_glowing, complementary_glowing = ligation(original_plasmid_cut_begin, gfp_original_cut,
+                                                       original_plasmid_cut_end)
     print(f'{original_glowing}\n{complementary_glowing}')
 
 
